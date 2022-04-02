66742f3fd2fbdea1a5806d0d126024597210bc55
julieta M <julietyartes2@gmail.com>
2022-04-02 10:50:34 -0500

    archivo de empleo

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let resultado;
+let Nombre = prompt('Cuál es nombre del empleado?');
+let valor = prompt('Cuantos hora pago?');
+let HorasTrabajadas = prompt('Cuantas horas trabajadas?');
+
+
+resultado=valor+HorasTrabajadas
+
+
+
+document.write(Nombre,valor,HorasTrabajadas);
+
